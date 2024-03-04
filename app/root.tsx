20a07a8d3cd3775791659d5ce62026d0fdd9898b
Sourav <sourav@ndtvs-MacBook-Air.local>
2024-03-04 19:44:23 +0530

    updating code

--- app/root.tsx
+++ app/root.tsx
@@ -58,19 +58,20 @@ export default function App() {
   return (
     <>
       <head>
-        <link rel="stylesheet" href={`/remix-beeps/styles/atf.css`} />
-        <link
-          rel="stylesheet"
-          href={`/remix-beeps/styles/atfBeepsDetail.css`}
-        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        {/*   <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin /> */}
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Noto+Sans+Devanagari:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
         <link rel="stylesheet" href="https://use.typekit.net/jkd2jqy.css" />
+        <link rel="stylesheet" href={`/remix-beeps/styles/atf.css`} />
+        <link
+          rel="stylesheet"
+          href={`/remix-beeps/styles/atfBeepsDetail.css`}
+        />
       </head>
       <body className="nav-trigger Vd-list Vd-Lst-pg">
         <SvgIcons />
