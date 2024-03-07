9ad703b3c7a8dab32ed5244ff555b96cf9da937d
Sourav <sourav@ndtvs-MacBook-Air.local>
2024-03-07 20:30:20 +0530

    jquery fix

--- app/root.tsx
+++ app/root.tsx
@@ -99,12 +99,14 @@ export default function App() {
       function updateHeight() {
         const ht = window.innerHeight;
         const svVertical2 = document.querySelector(".BepSl_cn");
+        if (svVertical2) {
         svVertical2.style.height = `${ht - 71}px`;
 
         if ($(window).width() <= 560) {
           svVertical2.style.height = `${ht}px`;
         }
       }
+    }
 
       if ($(window).width() <= 767) {
         updateHeight();
