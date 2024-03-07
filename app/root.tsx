4cd9c3e040c2d8102403e9cfe199383a679192ef
Sourav <sourav@ndtvs-MacBook-Air.local>
2024-03-07 20:04:30 +0530

    code update

--- app/root.tsx
+++ app/root.tsx
@@ -96,22 +96,22 @@ export function Layout({ children }: any) {
 export default function App() {
   useEffect(() => {
     $(document).ready(function () {
-    function updateHeight() {
-      const ht = window.innerHeight;
-      const svVertical2 = document.querySelector(".BepSl_cn");
-      svVertical2.style.height = `${ht - 71}px`;
+      function updateHeight() {
+        const ht = window.innerHeight;
+        const svVertical2 = document.querySelector(".BepSl_cn");
+        svVertical2.style.height = `${ht - 71}px`;
 
-      if ($(window).width() <= 560) {
-        svVertical2.style.height = `${ht}px`;
+        if ($(window).width() <= 560) {
+          svVertical2.style.height = `${ht}px`;
+        }
       }
-    }
 
-    if ($(window).width() <= 767) {
-      updateHeight();
-      window.addEventListener("resize", updateHeight, true);
-    }
-  });
-  },[]);
+      if ($(window).width() <= 767) {
+        updateHeight();
+        window.addEventListener("resize", updateHeight, true);
+      }
+    });
+  }, []);
   return (
     <>
       <SvgIcons />
