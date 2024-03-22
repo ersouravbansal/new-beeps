e6d38a28fb59906a8d9cde5260dec7ea1fd052c9
Sourav <sourav@ip-192-168-1-36.ap-south-1.compute.internal>
2024-03-22 11:56:10 +0530

    added time out

--- app/root.tsx
+++ app/root.tsx
@@ -155,29 +155,31 @@ export default function App() {
   }, [GTM_ID]);
   useEffect(() => {
     const logTriggerElements = document.querySelectorAll(".__log_trigger");
-    logTriggerElements.forEach(function (element) {
-      element.addEventListener("click", function () {
-        // alert("Hi Adnan")
-        if (!parent_c_islogin()) {
-          let __rurl = window.location.href;
-          window.location.href =
-            "https://stage-auth.ndtv.com/w/sso.html?siteurl=" +
-            encodeURIComponent(__rurl);
-        } else {
-          const toggleClass = element.getAttribute("data-class");
-          document.body.classList.toggle(toggleClass);
-        }
+    setTimeout(() => {
+      logTriggerElements.forEach(function (element) {
+        element.addEventListener("click", function () {
+           alert("Hi Adnan")
+          if (!parent_c_islogin()) {
+            let __rurl = window.location.href;
+            window.location.href =
+              "https://stage-auth.ndtv.com/w/sso.html?siteurl=" +
+              encodeURIComponent(__rurl);
+          } else {
+            const toggleClass = element.getAttribute("data-class");
+            document.body.classList.toggle(toggleClass);
+          }
+        });
       });
-    });
 
-    const overlaySideNav = document.querySelector(".overlay__side-nav");
-    const logSdCls = document.querySelector(".LogSd-cls");
-    overlaySideNav?.addEventListener("click", removeJsSideNavClass);
-    logSdCls?.addEventListener("click", removeJsSideNavClass);
+      const overlaySideNav = document.querySelector(".overlay__side-nav");
+      const logSdCls = document.querySelector(".LogSd-cls");
+      overlaySideNav?.addEventListener("click", removeJsSideNavClass);
+      logSdCls?.addEventListener("click", removeJsSideNavClass);
 
-    function removeJsSideNavClass() {
-      document.body.classList.remove("js_sid-nav-right");
-    }
+      function removeJsSideNavClass() {
+        document.body.classList.remove("js_sid-nav-right");
+      }
+    }, 5000);
   }, []);
   return (
     <>
