1a1983a78594c3f7e67ef6de8bbe89c7ad7bcf0e
Sourav <sourav@ip-192-168-1-37.ap-south-1.compute.internal>
2024-04-01 19:17:22 +0530

    30

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,7 +7,7 @@ import useStore from "~/stores/utilstore";
 import useInfiniteScroll from "~/hooks/useInfiniteScrollCustom";
 import { BASEPATH } from "~/constants";
 
-const pageSize = 50;
+const pageSize = 30;
 async function fetchVideos(pageNumber = 1) {
   const api_url =
     typeof process !== "undefined" ? process.env.REMIX_API_URL || "" : "";
