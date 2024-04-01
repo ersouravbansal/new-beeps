37a41fd7cdacc425939f1fd08b1905af6223db57
Sourav <sourav@ip-192-168-1-37.ap-south-1.compute.internal>
2024-04-01 14:14:49 +0530

    index api index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,9 +8,11 @@ import useInfiniteScroll from "~/hooks/useInfiniteScrollCustom";
 import { BASEPATH } from "~/constants";
 
 const pageSize = 40;
-
 async function fetchVideos(pageNumber = 1) {
-  const api_url = process.env.REMIX_API_URL || "";
+  const api_url =
+    typeof process !== "undefined" ? process.env.REMIX_API_URL || "" : "";
+  const basepath =
+    typeof process !== "undefined" ? process.env.REMIX_BASEPATH || "" : "";
   const response = await axios.get(
     `${api_url}${BASEPATH}/api/index1?pageNumber=${pageNumber}`
   );
@@ -19,6 +21,7 @@ async function fetchVideos(pageNumber = 1) {
 
 export const loader = async (params) => {
   try {
+    // const video_id = params.videoId?.split("-").pop();
     const response = await fetchVideos();
     if (!response.results) {
       throw new Response("Not Found", { status: 404 });
@@ -29,7 +32,6 @@ export const loader = async (params) => {
     return json({ total: "0", results: [] });
   }
 };
-
 export default function Index() {
   const urlupdate = useStore((state) => state.urlupdate);
   const setUrlupdate = useStore((state) => state.setUrlupdate);
@@ -39,10 +41,9 @@ export default function Index() {
   const [hasNextPage, setHasNextPage] = useState(true);
   const [error, setError] = useState();
   const [page, setPage] = useState(2);
-
   useEffect(() => {
-    if (!urlupdate) {
-      setUrlupdate(true);
+    if (urlupdate !== true) {
+      setUrlupdate((prev) => !prev);
     }
   }, [urlupdate]);
 
@@ -64,7 +65,16 @@ export default function Index() {
       setLoading(false);
     }
   }
+  // const [infiniteRef] = useInfiniteScroll({
+  //   loading,
+  //   onLoadMore: loadMore,
+  //   hasNextPage,
+  //   disabled: !!error,
+  //   rootMargin: "0px 0px 400px 0px",
+  // });
+  // console.log("Sourav Bansal",useInfiniteScroll)
 
+  
   const [infiniteRef, { rootRef }] = useInfiniteScroll({
     loading,
     hasNextPage,
@@ -75,103 +85,12 @@ export default function Index() {
 
   return (
     <>
-      <Content videoData={{ results: videos }} ref1={infiniteRef} ref2={rootRef} />
+      <Content
+        videoData={{ results: videos }}
+        ref1={infiniteRef}
+        ref2={rootRef}
+      />
     </>
   );
 }
-// import React, { useEffect, useState } from "react";
-// import { MetaFunction, json } from "@remix-run/node";
-// import Content from "~/components/Layout/Content";
-// import { useLoaderData } from "@remix-run/react";
-// import axios from "axios";
-// import useStore from "~/stores/utilstore";
-// import useInfiniteScroll from "~/hooks/useInfiniteScrollCustom";
-// import { BASEPATH } from "~/constants";
-
-// const pageSize = 40;
-// async function fetchVideos(pageNumber = 1) {
-//   const api_url =
-//     typeof process !== "undefined" ? process.env.REMIX_API_URL || "" : "";
-//   const basepath =
-//     typeof process !== "undefined" ? process.env.REMIX_BASEPATH || "" : "";
-//   const response = await axios.get(
-//     `${api_url}${BASEPATH}/api/index1?pageNumber=${pageNumber}`
-//   );
-//   return response.data || { total: "0", results: [] };
-// }
-
-// export const loader = async (params) => {
-//   try {
-//     // const video_id = params.videoId?.split("-").pop();
-//     const response = await fetchVideos();
-//     if (!response.results) {
-//       throw new Response("Not Found", { status: 404 });
-//     }
-//     return json(response);
-//   } catch (err) {
-//     console.error("Error:", err);
-//     return json({ total: "0", results: [] });
-//   }
-// };
-// export default function Index() {
-//   const urlupdate = useStore((state) => state.urlupdate);
-//   const setUrlupdate = useStore((state) => state.setUrlupdate);
-//   const { results: defaultVideos } = useLoaderData();
-//   const [videos, setVideos] = useState(defaultVideos);
-//   const [loading, setLoading] = useState(false);
-//   const [hasNextPage, setHasNextPage] = useState(true);
-//   const [error, setError] = useState();
-//   const [page, setPage] = useState(2);
-//   useEffect(() => {
-//     if (urlupdate !== true) {
-//       setUrlupdate((prev) => !prev);
-//     }
-//   }, [urlupdate]);
-
-//   async function loadMore() {
-//     setLoading(true);
-//     try {
-//       console.log("calling load more");
-//       const response = await fetchVideos(page);
-//       if (!response.results) {
-//         throw new Response("Not Found", { status: 404 });
-//       }
-//       setVideos((current) => [...current, ...response.results]);
-//       setPage((page) => page + 1);
-//       setHasNextPage(response.results.length === pageSize);
-//     } catch (err) {
-//       setError(err);
-//       console.error("Error:", err);
-//     } finally {
-//       setLoading(false);
-//     }
-//   }
-//   // const [infiniteRef] = useInfiniteScroll({
-//   //   loading,
-//   //   onLoadMore: loadMore,
-//   //   hasNextPage,
-//   //   disabled: !!error,
-//   //   rootMargin: "0px 0px 400px 0px",
-//   // });
-//   // console.log("Sourav Bansal",useInfiniteScroll)
-
-  
-//   const [infiniteRef, { rootRef }] = useInfiniteScroll({
-//     loading,
-//     hasNextPage,
-//     onLoadMore: loadMore,
-//     disabled: !!error,
-//     rootMargin: "0px 400px 0px 400px",
-//   });
-
-//   return (
-//     <>
-//       <Content
-//         videoData={{ results: videos }}
-//         ref1={infiniteRef}
-//         ref2={rootRef}
-//       />
-//     </>
-//   );
-// }
 
--- app/routes/api.index1.tsx
+++ app/routes/api.index1.tsx
@@ -1,13 +1,15 @@
+// ------------------------------------Index Route Videos API--------------------------------------
 import { json } from "@remix-run/node";
 import type { LoaderFunction } from "@remix-run/server-runtime";
 import axios from "axios";
 const pageSize = 40;
 
 let search_api_url = "";
-let search_api_key = "";
-
 if (typeof process !== "undefined") {
   search_api_url = process.env.REMIX_APP_BEEPS_SEARCH_API_BASE_URL || "";
+}
+let search_api_key = "";
+if (typeof process !== "undefined") {
   search_api_key = process.env.REMIX_APP_BEEPS_SEARCH_API || "";
 }
 
@@ -32,40 +34,3 @@ export const loader: LoaderFunction = async ({ request }) => {
     return json({ status: error });
   }
 };
-// // ------------------------------------Index Route Videos API--------------------------------------
-// import { json } from "@remix-run/node";
-// import type { LoaderFunction } from "@remix-run/server-runtime";
-// import axios from "axios";
-// const pageSize = 40;
-
-// let search_api_url = "";
-// if (typeof process !== "undefined") {
-//   search_api_url = process.env.REMIX_APP_BEEPS_SEARCH_API_BASE_URL || "";
-// }
-// let search_api_key = "";
-// if (typeof process !== "undefined") {
-//   search_api_key = process.env.REMIX_APP_BEEPS_SEARCH_API || "";
-// }
-
-// export const loader: LoaderFunction = async ({ request }) => {
-//   try {
-//     const url1 = new URL(request.url);
-//     const pageNumber = url1.searchParams.get("pageNumber");
-//     const BASE_URL = `https://${search_api_url}/video/mjson/client_key/${search_api_key}/`;
-//     const url = new URL(BASE_URL);
-//     url.searchParams.append(
-//       "extra_params",
-//       "ssl,show,gif,urltitle,source_id,fullimage,category,tmpcheck,newurl,ssl,sitelink,shorttitle,filepath,folder_path,preview,vertical"
-//     );
-//     url.searchParams.append("source", "1");
-//     url.searchParams.append("show_vertical", "1");
-//     url.searchParams.append("pageSize", pageSize + "");
-//     url.searchParams.append("pageNumber", pageNumber);
-//     url.searchParams.append("video_format", "allformat");
-//     const response = await axios.get(url.href);
-//     return json(response.data);
-//   } catch (error) {
-//     return json({ status: error });
-//   }
-// };
-
